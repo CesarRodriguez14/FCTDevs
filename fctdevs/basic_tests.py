4c8cd0f55c2da51fa8575280dffb62d048b85d5f
CesarRodriguez14 <cesrodflo14@hotmail.com>
2025-12-22 11:59:11 -0600

    [Feature][v0.2.0]-Add log to electric test, update return of led_test(return 3 flags)

--- fctdevs/basic_tests.py
+++ fctdevs/basic_tests.py
@@ -1,16 +1,22 @@
 class BasicTests():
-    def electric_test(self,_val,_max=None,_min=None):
+    def electric_test(self,_val,_max=None,_min=None,_logFlag=False):
         result = True
+        log = ''
         if _max != None:
+            log.append(f'MAX:{_max}\t')
             result = _val <= _max and result
         if _min != None:
+            log.append(f'MIN:{_min}\t')
             result = _val >= _min and result
+        log.append(f'VAL:{_val}')
+        if _logFlag:
+            print(log)
         return result
     
     def led_test(self,_rgb,_max,_min):
-        result = True
+        result = []
         for i,color in enumerate(_rgb):
-            result = self.electric_test(color,_max[i],_min[i]) and result
+            result.append(self.electric_test(color,_max[i],_min[i]))
         return result
             
 
