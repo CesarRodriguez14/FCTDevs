5a464c95f059a205ed2b334568ecd14152ef6640
CesarRodriguez14 <cesrodflo14@hotmail.com>
2026-01-20 10:43:31 -0600

    [Feature][v0.3.1]-Add IOport lib class and Digital Input and Output classes. Update Relay, ANDON, BlockElement. Add PistonPisador, PistonPines, PistonPinesProg, BoardDetect, PCBDetect

--- fctdevs/fct_devs.py
+++ fctdevs/fct_devs.py
@@ -49,39 +49,6 @@ class PowerSource():
     def close(self):
         self.instrument.close()
 
-class BlockElement():
-    def __init__(self, _canal:int, _daq:DAQ):
-        self.canal = _canal
-        self.daq = _daq
-
-    def block(self):
-        self.daq.close_channel(self.canal)
-    
-    def unblock(self):
-        self.daq.open_channel(self.canal)
-
-class ANDON():
-    def __init__(self,_canal:int,_daq:DAQ):
-        self.canal = _canal
-        self.daq = _daq
-
-    def turn_on(self):
-        self.daq.close_channel(self.canal)
-
-    def turn_off(self):
-        self.daq.open_channel(self.canal)
-
-class Relay():
-    def __init__(self,_canal:int,_daq:DAQ):
-        self.canal = _canal
-        self.daq = _daq
-
-    def open(self):
-        self.daq.open_channel(self.canal)
-
-    def close(self):
-        self.daq.close_channel(self.canal)
-
 class Cover():
     def __init__(self,_canal:int,_daq:DAQ):
         self.canal = _canal
@@ -186,5 +153,178 @@ class FEASA():
             return False
         return True
     
+class IOCard():
+    OK_RESULT = "OK"
+    INDEX_RED = 0
+    INDEX_GREEN = 1
+    INDEX_BLUE = 2
+    def __init__(self):
 
+        # str that saves COM port
+        self.COM_port = None
+
+        self.io = ctypes.cdll.LoadLibrary(r".\ioportlib.dll")
+
+        # str = io.Err()
+        self.io.io_Err.restype = ctypes.c_char_p
+
+        # io.Open("COM1")
+        self.io.io_Open.restype = ctypes.c_bool
+        self.io.io_Open.argtypes = [ctypes.c_char_p]
+
+        # io.Close()
+        self.io.io_Close.restype = ctypes.c_void_p
+
+        # Res = io.dim16Val(ModuleNum,InpNum)
+        self.io.io_dim16Val.restype = ctypes.c_bool
+        self.io.io_dim16Val.argtypes = [ctypes.c_int, ctypes.c_int]
+
+        # io.dom16Val(ModuleNum,OutNum,OutVal)
+        
+        self.io.io_dom16Val.restype = ctypes.c_void_p
+        self.io.io_dom16Val.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_bool]
+
+        # Res = io.lim16Val(ModuleNum,InpNum)
+        self.io.io_lim16Val.restype = ctypes.c_bool
+        self.io.io_lim16Val.argtypes = [ctypes.c_int, ctypes.c_int]
+
+        # Res = io.rgbimVal(ModuleNum,InpNum,ColorNum)
+        self.io.io_rgbimVal.restype = ctypes.c_int
+        self.io.io_rgbimVal.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int]
+
+        # Res = io.Update(ModuleNam,ModuleNum)
+        self.io.io_Update.restype = ctypes.c_bool
+        self.io.io_Update.argtypes = [ctypes.c_char_p, ctypes.c_int]
+
+    def set_port(self, _port:int):
+            self.COM_port = _port
+
+    def open(self):
+        result = self.OK_RESULT
+        if not self.io.io_Open(self.COM_port.encode('utf-8')):
+            result = self.io.io_Err().decode('utf-8')
+        return result
     
+    def close(self):
+        self.io.io_Close()
+        return self.OK_RESULT
+    
+    def output(self,_module:int,_outCanal:int,_value:bool):
+        self.io.io_dom16Val(_module,_outCanal,_value)
+        result = self.io.io_Update("dom16",_module)
+        if result:
+            return self.OK_RESULT
+        else:
+            return self.io.io_Err()
+        
+    def input(self,_module:int,_inCanal:int):
+        update_val = self.io.io_Update("dim16",_module)
+        if update_val == True:
+            return [self.OK_RESULT,self.io.io_dim16Val(_module,_inCanal)]
+        else:
+            return [self.io.io_Err(),False]
+        
+    def rgbim(self,_module:int,_canal:int):
+        update_val = self.io.io_Update("rgbim",_module)
+        if update_val == True:
+            leds = [
+                self.io.io_rgbimVal(_module,_canal,self.INDEX_RED),
+                self.io.io_rgbimVal(_module,_canal,self.INDEX_GREEN),
+                self.io.io_rgbimVal(_module,_canal,self.INDEX_BLUE)]
+            
+            return [self.OK_RESULT,leds]
+        else:
+            return [self.io.io_Err(),[-1]*3]
+    
+class DigitalOutputDev():
+    HANDLER_DAQ = 0
+    HANDLER_IOCard = 1
+    OK_RESULT = IOCard.OK_RESULT
+    def __init__(self,_modulo:int,_canal:int,_handler,_handlerType:int=HANDLER_DAQ):
+        self.handlerType = _handlerType
+        self.handler = _handler
+        self.canal = _canal
+        self.modulo = _modulo
+    def setON(self):
+        if self.handlerType == self.HANDLER_DAQ:
+            self.handler.close_channel(self.canal)
+            return self.OK_RESULT
+        if self.handlerType == self.HANDLER_IOCard:
+            return self.handler.output(self.modulo,self.canal,True)
+        
+    def setOFF(self):
+        if self.handlerType == self.HANDLER_DAQ:
+            self.handler.open_channel(self.canal)
+            return self.OK_RESULT
+        if self.handlerType == self.HANDLER_IOCard:
+            return self.handler.output(self.modulo,self.canal,False)
+        
+class BlockElement(DigitalOutputDev):
+    def __init__(self,_modulo:int,_canal:int,_handler,_handlerType:int=DigitalOutputDev.HANDLER_DAQ):
+        super().__init__(_modulo,_canal,_handler,_handlerType)
+
+    def block(self):
+        self.setON()
+    
+    def unblock(self):
+        self.setOFF()
+
+class ANDON(DigitalOutputDev):
+    def __init__(self,_modulo:int,_canal:int,_handler,_handlerType:int=DigitalOutputDev.HANDLER_DAQ):
+        super().__init__(_modulo,_canal,_handler,_handlerType)
+
+    def turn_on(self):
+        self.setON()
+    
+    def turn_off(self):
+        self.setOFF()
+
+class Relay(DigitalOutputDev):
+    def __init__(self,_modulo:int,_canal:int,_handler,_handlerType:int=DigitalOutputDev.HANDLER_DAQ):
+        super().__init__(_modulo,_canal,_handler,_handlerType)
+
+    def energize(self):
+        self.setON()
+
+    def deenergize(self):
+        self.setOFF()
+
+class PistonPisador(Relay):
+    def __init__(self,_modulo:int,_canal:int,_handler,_handlerType:int=DigitalOutputDev.HANDLER_DAQ):
+        super().__init__(_modulo,_canal,_handler,_handlerType)
+
+class PistonPines(Relay):
+    def __init__(self,_modulo:int,_canal:int,_handler,_handlerType:int=DigitalOutputDev.HANDLER_DAQ):
+        super().__init__(_modulo,_canal,_handler,_handlerType)
+
+class PistonPinesProg(Relay):
+    def __init__(self,_modulo:int,_canal:int,_handler,_handlerType:int=DigitalOutputDev.HANDLER_DAQ):
+        super().__init__(_modulo,_canal,_handler,_handlerType)
+
+class DigitalInputDev():
+    HANDLER_DAQ = 0
+    HANDLER_IOCard = 1
+    OK_RESULT = IOCard.OK_RESULT
+    NOT_DEV = "NOT_DEV"
+    def __init__(self,_modulo:int,_canal:int,_handler,_handlerType:int=HANDLER_IOCard):
+        self.handlerType = _handlerType
+        self.handler = _handler
+        self.canal = _canal
+        self.modulo = _modulo
+
+    def getState(self):
+        if self.handlerType == self.HANDLER_IOCard:
+            return self.handler.input(self.modulo,self.canal)
+        else:
+            return [self.NOT_DEV,False]
+        
+class BoardDetect(DigitalInputDev):
+    def __init__(self, _modulo:int, _canal:int, _handler, _handlerType = DigitalInputDev.HANDLER_IOCard):
+        super().__init__(_modulo, _canal, _handler, _handlerType)
+
+    def isDetected(self):
+        return self.getState()
+    
+class PCBDetect(BoardDetect):
+    def __init__(self, _modulo:int, _canal:int, _handler, _handlerType = DigitalInputDev.HANDLER_IOCard):
+        super().__init__(_modulo, _canal, _handler, _handlerType)
