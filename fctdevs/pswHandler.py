b8f9abda3967dbead188ec9a907f319768b2bf9d
CesarRodriguez14 <cesrodflo14@hotmail.com>
2026-04-13 10:29:03 -0600

    [Update][v0.8.3]-Actualizar revisión de estado de PSW

--- fctdevs/pswHandler.py
+++ fctdevs/pswHandler.py
@@ -1,7 +1,6 @@
 import json
 
 class pswHandler():
-    PSW_FAIL_FLAG = -1
     PSW_USER_FLAG = 0
     PSW_PSW_FLAG = 1
     def __init__(self,_psw_file:str):
@@ -9,21 +8,24 @@ class pswHandler():
         self.last_user_to_validate = "UNDEFINED"
         self.getPSW()
 
-    def getPSW(self,_last_fails_count:int):
-        self.state = self.PSW_PSW_FLAG
+    def getPSW(self):
+        self.state = True
         try:
             with open(self.psw_file,'r',encoding = 'utf-8') as _file:
                 _data = json.load(_file)
                 self.psw = _data["psw"]
                 self.users = _data["users"]
                 self.max_fails = _data["max_fails"]
-            if _last_fails_count >= self.max_fails:
-                self.state = self.PSW_USER_FLAG
-            else:
-                self.state = self.PSW_PSW_FLAG
         except:
-            self.state = self.PSW_FAIL_FLAG
+            self.state = False
         return self.state
+    
+    def getPSWType(self,_last_fails_count:int):
+        if _last_fails_count >= self.max_fails:
+            self.state = self.PSW_USER_FLAG
+        else:
+            self.state = self.PSW_PSW_FLAG
+
 
     def validatePSW(self,_last_fails_count:int,_psw:str):
         if _last_fails_count >= self.max_fails:
