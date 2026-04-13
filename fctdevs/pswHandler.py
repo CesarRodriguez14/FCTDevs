be53221ff652d3adb5fb5020246278303625cff5
CesarRodriguez14 <cesrodflo14@hotmail.com>
2026-04-13 10:42:34 -0600

    [Update][v0.8.4]-Agregar self.psw_type y separarla de self.state

--- fctdevs/pswHandler.py
+++ fctdevs/pswHandler.py
@@ -1,38 +1,45 @@
 import json
 
 class pswHandler():
+    PSW_NO_READ_FLAG = -1
     PSW_USER_FLAG = 0
     PSW_PSW_FLAG = 1
     def __init__(self,_psw_file:str):
         self.psw_file = _psw_file
         self.last_user_to_validate = "UNDEFINED"
+        self.state = False
         self.getPSW()
 
     def getPSW(self):
-        self.state = True
         try:
             with open(self.psw_file,'r',encoding = 'utf-8') as _file:
                 _data = json.load(_file)
                 self.psw = _data["psw"]
                 self.users = _data["users"]
                 self.max_fails = _data["max_fails"]
+                self.state = True
         except:
             self.state = False
         return self.state
     
     def getPSWType(self,_last_fails_count:int):
-        if _last_fails_count >= self.max_fails:
-            self.state = self.PSW_USER_FLAG
+        if self.state:
+            if _last_fails_count >= self.max_fails:
+                self.psw_type = self.PSW_USER_FLAG
+            else:
+                self.psw_type = self.PSW_PSW_FLAG
         else:
-            self.state = self.PSW_PSW_FLAG
+            self.psw_type = self.PSW_NO_READ_FLAG
+        return self.psw_type
 
 
     def validatePSW(self,_last_fails_count:int,_psw:str):
-        if _last_fails_count >= self.max_fails:
+        self.getPSWType(_last_fails_count)
+        if self.psw_type == self.PSW_USER_FLAG:
             res_flag = _psw in self.users
             if res_flag:
                 self.last_user_to_validate = _psw
-        else:
+        elif self.psw_type == self.PSW_PSW_FLAG:
             res_flag = _psw == self.psw
         return res_flag
 
